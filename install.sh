b487ed468764e41d4e406f1adbc4525d2693fd52
Andrey Sharypov <sharypovandrey@gmail.com>
2020-04-09 00:03:19 +0700

    update

--- install.sh
+++ install.sh
@@ -24,8 +24,8 @@ systemctl reload nginx
 
 bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
 
-sed -i 's^	# bind to = *^	bind to = 127.0.0.1' /etc/netdata/netdata.conf
+sed -i '0,/	# bind to = */s//	bind to = 127.0.0.1/' /etc/netdata/netdata.conf
 
-sed -i 's^   # Handle RTMPT (RTMP Tunneling).  Forwards requests^  # Netdata Monitoring\n  location /netdata/ {\n    auth_basic "BigBlueButton";\n    auth_basic_user_file /etc/nginx/.htpasswd;\n    proxy_pass         http://127.0.0.1:19999/;\n    proxy_redirect     default;\n    proxy_set_header   X-Forwarded-For   $proxy_add_x_forwarded_for;\n    client_max_body_size       10m;\n    client_body_buffer_size    128k;\n    proxy_connect_timeout      90;\n    proxy_send_timeout         90;\n    proxy_read_timeout         90;\n    proxy_buffer_size          4k;\n    proxy_buffers              4 32k;\n    proxy_busy_buffers_size    64k;\n    proxy_temp_file_write_size 64k;\n    include    fastcgi_params;\n  }\n\n  # Handle RTMPT (RTMP Tunneling).  Forwards requests^' /etc/nginx/sites-available/bigbluebutton
+sed -i 's^  # Handle RTMPT (RTMP Tunneling).  Forwards requests^  # Netdata Monitoring\n  location /netdata/ {\n    auth_basic "BigBlueButton";\n    auth_basic_user_file /etc/nginx/.htpasswd;\n    proxy_pass         http://127.0.0.1:19999/;\n    proxy_redirect     default;\n    proxy_set_header   X-Forwarded-For   $proxy_add_x_forwarded_for;\n    client_max_body_size       10m;\n    client_body_buffer_size    128k;\n    proxy_connect_timeout      90;\n    proxy_send_timeout         90;\n    proxy_read_timeout         90;\n    proxy_buffer_size          4k;\n    proxy_buffers              4 32k;\n    proxy_busy_buffers_size    64k;\n    proxy_temp_file_write_size 64k;\n    include    fastcgi_params;\n  }\n\n  # Handle RTMPT (RTMP Tunneling).  Forwards requests^' /etc/nginx/sites-available/bigbluebutton
 
 systemctl reload nginx
